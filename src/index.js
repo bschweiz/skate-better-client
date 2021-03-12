3056c4a4b1accc93808d58c8f0eabd3786c22ce5
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-12 11:10:24 -0600

    installed react-router-dom dependency

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { SKATEBetter } from './components/SKATEBetter.js';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SKATEBetter />
   </React.StrictMode>,
   document.getElementById('root')
 );
