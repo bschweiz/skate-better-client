8c9062ec11ab11e4108ca8ea565f06c1ff807b49
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-16 15:24:27 -0500

    changed stance to a checkbox, need to recnsider the UI on this view and add a button above input fields for new opponent to start the game with a previous opponent. now i also need to start working on building out the viewset for games so i can initiate a game

--- src/components/opponent/OpponentSelect.js
+++ src/components/opponent/OpponentSelect.js
@@ -1,40 +1,61 @@
-import React, { useContext, useRef, useEffect } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { OpponentContext } from "./OpponentProvider"
 
 export const OpponentSelect = (props) => {
 
     const { allOpponents, addOpponent, getAllOpponents } = useContext(OpponentContext)
 
+    const [currentOpponent, setCurrentOpponent] = useState({
+        handle: '',
+        goofy: false
+    })
+
     useEffect(() => {
         getAllOpponents()
     }, [])
 
-  
+    const changeOpponentState = (DOMEvent) => {
+        const newOpponentState = Object.assign({}, currentOpponent)
+
+        newOpponentState[DOMEvent.target.name] = DOMEvent.target.value
+        setCurrentOpponent(newOpponentState)
+        console.log(currentOpponent)
+    }
 
 
- 
+
 
     return (
         <form className="opponentSelect">
             <fieldset>
                 <div className="form-group">
 
-                    <select className="form-control" autoFocus 
+                    <select className="form-control" type="text" name="handle" autoFocus
+                        onChange={changeOpponentState}
                     >
-                        <option value='0'>Previous Opponents</option>
+                        <option value='0'>Choose a Previous Opponent</option>
                         {allOpponents.map((o) => (
-                            <option key={o.id} value={o.id}>{o.handle}</option> 
+                            <option key={o.id} value={o.id}>{o.handle}</option>
                         ))}
                     </select>
                 </div>
             </fieldset>
 
             <fieldset>
-                <input type="text" required autoFocus placeholder="enter name" />
+                <div className="form-group">
+                    <label htmlFor="maker">Name or Nickname: </label>
+                    <input type="text" name="handle" required autoFocus className="form-control"
+                        value={currentOpponent.handle}
+                        onChange={changeOpponentState}
+                    />
+                </div>
             </fieldset>
 
             <fieldset>
-                <input type="text" required autoFocus placeholder="stance" />
+                <div className="form-group">
+                    <label htmlFor="goofyStatus">Goofy-footed?  </label>
+                    <input type="checkbox" id="goofySelect" required autoFocus placeholder="stance" />
+                </div>
             </fieldset>
 
             <button type="submit"
