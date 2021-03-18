4ca177dfa17f7578a3b870ab6a3033f3e032b430
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-18 14:27:06 -0500

    removed commented code

--- src/components/game/PastGameCard.js
+++ src/components/game/PastGameCard.js
@@ -22,21 +22,3 @@ export const PastGameCard = ({ game }) => {
 
 
 
-
-// return (
-
-//     <section className="game__info">
-//         <Link className="card-link"
-//             to={{
-//                 pathname: `/reviewgame/${game.id}`,
-//                 state: { chosenGame: game }
-//             }}>
-//         <h2 className="card-title">Choose This Game</h2>
-//         </Link>
-//         <h3 className="common__name">{game.date_time}</h3>
-//         <div className="scientific__name">Won ?{game.won}</div>
-//         <div className="scientific__name">Where ?{game.location}</div>
-
-
-//     </section>
-// )
