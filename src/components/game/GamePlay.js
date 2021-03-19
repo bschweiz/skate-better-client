3a54d55e540c682b49f5693039bb9758ab850610
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-19 12:53:23 -0500

    at least it is not breaking yet on /play view, about to test gameplay view with new trickSelect

--- src/components/game/GamePlay.js
+++ src/components/game/GamePlay.js
@@ -9,6 +9,10 @@ export const GamePlay = (props) => {
 
     const [ availableTricks, setAvailableTricks ] = useState([])
     
+    const [currentTrick, setCurrentTrick] = useState({
+        trickId: 0,
+    })
+    
     useEffect(() => {
         console.log(props)
         getAllTricks()
@@ -18,23 +22,31 @@ export const GamePlay = (props) => {
         setAvailableTricks()
     }, [allTricks])
     
+    const changeCurrentTrick = (DOMEvent) => {
+        const newTrickState = Object.assign({}, currentTrick)
+
+        newTrickState[DOMEvent.target.name] = DOMEvent.target.value
+        setCurrentTrick(newTrickState)
+    }
     
     
         return (
+            <>
             <h2>Which trick? (Available Tricks)</h2>
             <fieldset>
                 <div className="form-group">
 
-                    <select className="form-control" type="text" name="opponentId" autoFocus
-                        onChange={changeGameDetails}
+                    <select className="form-control" type="text" name="trickSelect" autoFocus
+                        onChange={changeCurrentTrick}
                     >
-                        <option value='0'>Previous Opponents</option>
-                        {allOpponents.map(o => (
-                            <option key={o.id} value={o.id}>{o.handle}</option>
+                        <option value='0'>Available Tricks</option>
+                        {availableTricks.map(tr => (
+                            <option key={tr.id} value={tr.id}>{tr.name}</option>
                         ))}
                     </select>
                 </div>
             </fieldset>
+
             <h2>Full Trick List:</h2>
             <div className="tricks"> <h3>Tricks List</h3>
             
@@ -43,7 +55,9 @@ export const GamePlay = (props) => {
                         return <TrickCard key={t.id} trick={t} props={props}/>
                     })
                 }
-            </div>)
+            </div>
+            </>
+            )
     }
         
         
