6e8d42f736439089948877b39a430c8106e39a37
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-20 17:43:09 -0500

    cleaning up code on GameTrickCard.js

--- src/components/gametrick/GameTrickCard.js
+++ src/components/gametrick/GameTrickCard.js
@@ -1,44 +1,25 @@
-import React, { useContext, useEffect } from "react"
-import { Link } from "react-router-dom"
-import { GameTrickContext } from "./GameTrickProvider"
+import React from "react"
+// import React, { useContext, useEffect } from "react"
+// import { Link } from "react-router-dom"
+// import { GameTrickContext } from "./GameTrickProvider"
 import "./GameTrickCard.css"
 
 
 
 export const GameTrickCard = ({ gametrick, props }) => {
-
-    // const { tricks, getTricks } = useContext(TrickContext)
-    // // debugger
-    // useEffect(() => {
-    //     getTricks()
-    // }, [])
-
-    // const matchingPlant = plants.find(p => p.id === event.plantId)
-    
-    // const checkboxControl = (evt) => {
-    //     if(evt.target.checked === true){
-    //         updateCompleted(parseInt(evt.target.id), {complete: true})
-    //     } else {
-    //         updateCompleted(parseInt(evt.target.id), {complete: false})
-    //     }
-    // }
-    
     
         return (
             <section className="trick_info">
         
                     <input type="checkbox" key={"skater-make" + gametrick.id} name="checkbox" checked=
                     { gametrick.user_make ? "checked": ""}
-                    onChange={evt=>{
-                        console.log(evt)
-                    }} />
+                    />
                     <label htmlFor={gametrick.id}>{gametrick.trick.name}</label>
                     <input type="checkbox" key={"opponent-make" + gametrick.id} name="checkbox" checked=
                     { gametrick.opponent_make ? "checked": ""}
-                    onChange={evt=>{
-                        console.log(evt)
-                    }} />
+                    />
             </section>
         )
     
 }
+
