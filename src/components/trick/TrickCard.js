63fe76daab1ca5ffaccb5f67e4186acaa3876626
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-20 15:35:38 -0500

    i think it might be working

--- src/components/trick/TrickCard.js
+++ src/components/trick/TrickCard.js
@@ -7,60 +7,71 @@ import "./TrickCard.css"
 
 export const TrickCard = ({ trick, gameId, props }) => {
 
-    const [ trickLandedState, setTrickLandedState ] = useState({
+    const [trickLandedState, setTrickLandedState] = useState({
         userMake: 0,
         opponentMake: 0,
     })
 
-    const changeUserMake = (DOMEvent) => {
+    const changeUserMake = (evt) => {
         const newTrickState = Object.assign({}, trickLandedState)
-
-        newTrickState[DOMEvent.target.name] = DOMEvent.target.value
+        if (evt.target.checked === true) {
+            newTrickState[evt.target.name] = true
+        } else {
+            newTrickState[evt.target.name] = false
+        }
         setTrickLandedState(newTrickState)
     }
 
-    const changeOpponentMake = (DOMEvent) => {
-        const newTrickState = Object.assign({}, trickLandedState)
+    // const changeOpponentMake = (DOMEvent) => {
+    //     const newTrickState = Object.assign({}, trickLandedState)
 
-        newTrickState[DOMEvent.target.name] = DOMEvent.target.value
-        setTrickLandedState(newTrickState)
-    }
-    
+    //     newTrickState[DOMEvent.target.name] = DOMEvent.target.value
+    //     setTrickLandedState(newTrickState)
+    // }
+
+    // const UsercheckboxControl = (evt) => {
+    //     if (evt.target.checked === true) {
+    //         setTrickLandedState(parseInt(evt.target.id), { complete: true })
+    //     } else {
+    //         updateCompleted(parseInt(evt.target.id), { complete: false })
+    //     }
+    // }
+
     useEffect(() => {
         console.log("trick:", trick, 'gameId: ', gameId, 'trickLandedState: ', trickLandedState)
     }, [trick, trickLandedState])
-        return (
-            <section className="trick_info">
-        
-                    <input type="checkbox" key="user-make" id={trick.id} name="userMake" 
-                    
-                    checked={trickLandedState.userMake ? "checked": ""}
-                    onChange={evt=>{
-                        console.log(evt)
-                    }} 
-                        
-                    />
-                    <label htmlFor={trick.id}>{trick.name}</label>
-                    <input type="checkbox" key="opponent-make" id={trick.id} name="opponenteMake" 
+    return (
+        <section className="trick_info">
 
-                    checked={trickLandedState.opponentMake ? "checked": ""}
-                    onChange={evt=>{
-                        console.log(evt)
-                    }} 
+            <input type="checkbox" key="user-make" id={trick.id} name="userMake"
 
-                    />
-                    <button 
-                    onClick={evt=> {
-                        evt.preventDefault()
+                checked={trickLandedState.userMake ? "checked" : ""}
+                onChange={evt => {
+                    changeUserMake(evt)
+                }}
 
-                        const newGameTrick = {
-                            gameId: gameId,
-                            trickId: parseInt(trick.id),
-                            userMake: trickLandedState.userMake,
-                            opponentMake: trickLandedState.opponentMake
-                        }
-                    }}>next trick</button>
-            </section>
-        )
-    
+            />
+            <label htmlFor={trick.id}>{trick.name}</label>
+            <input type="checkbox" key="opponent-make" id={trick.id} name="opponenteMake"
+
+                checked={trickLandedState.opponentMake ? "checked" : ""}
+                onChange={evt => {
+                    changeUserMake(evt)
+                }}
+
+            />
+            <button
+                onClick={evt => {
+                    evt.preventDefault()
+
+                    const newGameTrick = {
+                        gameId: gameId,
+                        trickId: parseInt(trick.id),
+                        userMake: trickLandedState.userMake,
+                        opponentMake: trickLandedState.opponentMake
+                    }
+                }}>next trick</button>
+        </section>
+    )
+
 }
