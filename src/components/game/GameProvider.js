909b5f7d62945bdbad9bd3f20d5291df7a9ac222
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-22 12:30:10 -0500

    making progress on GameProvider, need to break for lunch

--- src/components/game/GameProvider.js
+++ src/components/game/GameProvider.js
@@ -6,8 +6,19 @@ export const GameContext = React.createContext()
 export const GameProvider = (props) => {
 
     const [allGames, setAllGames] = useState([])
+    const [currentGame, setAllGames] = useState([])
     const [newestGameId, setNewestGameId] = useState([])
 
+    const getAllGames = () => {
+        return fetch("http://localhost:8000/game", {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("sb_token")}`
+            }
+        })
+            .then(response => response.json())
+            .then(setAllGames)
+    }
+    
     const getAllGames = () => {
         return fetch("http://localhost:8000/game", {
             headers: {
@@ -47,7 +58,7 @@ export const GameProvider = (props) => {
 
     return (
         <GameContext.Provider value={{
-            allGames, newestGameId, getAllGames, createGame, createNewOpponentGame
+            allGames, currentGame, newestGameId, getAllGames, createGame, createNewOpponentGame
         }}>
             {props.children}
         </GameContext.Provider>
