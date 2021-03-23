0692de15a3fedef1f0cee2f3e6b58f7d5c2df463
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-23 14:26:31 -0500

    just going to start by putting an "edit trick" button on the GameTrickCard

--- src/components/gametrick/GameTrickCard.js
+++ src/components/gametrick/GameTrickCard.js
@@ -7,19 +7,19 @@ import "./GameTrickCard.css"
 
 
 export const GameTrickCard = ({ gametrick, props }) => {
-    
+
         return (
-            <section className="trick_info">
-        
-                    <input type="checkbox" key={"skater-make" + gametrick.id} name="checkbox" checked=
-                    { gametrick.user_make ? "checked": ""}
-                    />
-                    <label htmlFor={gametrick.id}>{gametrick.trick.name}</label>
-                    <input type="checkbox" key={"opponent-make" + gametrick.id} name="checkbox" checked=
-                    { gametrick.opponent_make ? "checked": ""}
-                    />
-            </section>
+                <section className="trick_info">
+
+                        <input type="checkbox" key={"skater-make" + gametrick.id} name="checkbox" checked=
+                                {gametrick.user_make ? "checked" : ""}
+                        />
+                        <label htmlFor={gametrick.id}>{gametrick.trick.name}</label>
+                        <input type="checkbox" key={"opponent-make" + gametrick.id} name="checkbox" checked=
+                                {gametrick.opponent_make ? "checked" : ""}
+                        />
+                </section>
         )
-    
+
 }
 
