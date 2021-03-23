716b9326bcb06d2e327a3efacb9ec7fde70ea76f
Benjamin Schweizer <bschweiz@gmail.com>
2021-03-23 16:17:04 -0500

    trying to conditionally render the dropdown to select a new trick

--- src/components/gametrick/GameTrickCard.js
+++ src/components/gametrick/GameTrickCard.js
@@ -1,5 +1,5 @@
 
-import React, { useContext, useState } from "react"
+import React, { useContext, useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import { GameTrickContext } from "./GameTrickProvider"
 import { TrickContext } from "../trick/TrickProvider"
@@ -8,11 +8,17 @@ import "./GameTrickCard.css"
 
 
 export const GameTrickCard = ({ gametrick, props }) => {
-        const { getAvailableTrickByGame, availableTricks } = useContext(TrickContext)
+        const { getAvailableTricksByGame, availableTricks } = useContext(TrickContext)
+
+        let editMode = false
 
         const [chosenGameTrick, setChosenGameTrick] = useState({
                 trickId: 0,
         })
+
+        useEffect(() => {
+                getAvailableTrickByGame()
+        }, [])
 
         return (
                 <section className="trick_info">
@@ -26,22 +32,24 @@ export const GameTrickCard = ({ gametrick, props }) => {
                         />
                         <button className="edit_trick"
                                 onClick={() => {
+                                        editMode = true
                                         console.log(gametrick)
+                                        return editMode
                                 }}>change trick</button>
-                        { availableTricks ?
-                <fieldset>
-                    <div className="form-group">
+                        { editMode ?
+                                <fieldset>
+                                        <div className="form-group">
 
-                        <select className="form-control" type="text" name="trickId" autoFocus
-                            onChange={changeCurrentTrick}
-                        >
-                            <option value='0'>Available Tricks</option>
-                            {availableTricks.map(tr => (
-                                <option key={tr.id} value={tr.id}>{tr.name}</option>
-                            ))}
-                        </select>
-                    </div>
-                </fieldset> : <div></div>}
+                                                <select className="form-control" type="text" name="trickId" autoFocus
+                                                        onChange={console.log(availableTricks)}
+                                                >
+                                                        <option value='0'>Available Tricks</option>
+                                                        {availableTricks.map(tr => (
+                                                                <option key={tr.id} value={tr.id}>{tr.name}</option>
+                                                        ))}
+                                                </select>
+                                        </div>
+                                </fieldset> : <div></div>}
                 </section>
         )
 
