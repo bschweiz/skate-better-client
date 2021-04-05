8986d0388a29f88a8223292845f849c786e9d254
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-05 17:28:23 -0500

    ok now  working properly to change the letters for user score, my issue was one equals sign instead of 2

--- src/components/score/ScoreCard.js
+++ src/components/score/ScoreCard.js
@@ -1,16 +1,26 @@
 
 import React, { useContext, useState } from "react"
-import { TrickContext } from "../trick/TrickProvider"
 
 
 
 
 export const ScoreCard = ({ score, props }) => {
-        
+        console.log("score from ScoreCard", score)
+        let SKATEState = "__.__.__.__.__."
+        debugger
+        if (score == 0) {return SKATEState}
+        else if (score == 1) {SKATEState = "S_.__.__.__.__."}
+        else if (score == 2) {SKATEState = "S_.K_.__.__.__."}
+        else if (score == 3) {SKATEState = "S_.K_.A_.__.__."}
+        else if (score == 4) {SKATEState = "S_.K_.A_.T_.__."}
+        else {SKATEState = "S_.K_.A_.T_.E_."}
 
         return (
                 <>
-                        <h2>test, user score: {score}</h2>
+                        {
+                                <h2>{SKATEState}</h2>
+                                
+                                }
                 </>
                 // <section className="trick_info">
 
