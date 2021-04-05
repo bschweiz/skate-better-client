a6d8b9a428d5c9865f8a4de5a7befa3bf2a6fffe
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-05 17:12:18 -0500

    user score in new ScoreCard element updating and rendering OK now. next task is to replicate and create the opponent scorecard

--- src/components/score/ScoreCard.js
+++ src/components/score/ScoreCard.js
@@ -5,21 +5,24 @@ import { TrickContext } from "../trick/TrickProvider"
 
 
 
-export const ScoreCard = ({ gametrick, props }) => {
+export const ScoreCard = ({ score, props }) => {
         
 
         return (
-                <section className="trick_info">
+                <>
+                        <h2>test, user score: {score}</h2>
+                </>
+                // <section className="trick_info">
 
-                        <input type="checkbox" key={"skater-make" + gametrick.id} name="checkbox" checked=
-                                {gametrick.user_make ? "checked" : ""}
-                        />
-                        <label htmlFor={gametrick.id}>{gametrick.trick.name}</label>
-                        <input type="checkbox" key={"opponent-make" + gametrick.id} name="checkbox" checked=
-                                {gametrick.opponent_make ? "checked" : ""}
-                        />
+                //         <input type="checkbox" key={"skater-make" + gametrick.id} name="checkbox" checked=
+                //                 {gametrick.user_make ? "checked" : ""}
+                //         />
+                //         <label htmlFor={gametrick.id}>{gametrick.trick.name}</label>
+                //         <input type="checkbox" key={"opponent-make" + gametrick.id} name="checkbox" checked=
+                //                 {gametrick.opponent_make ? "checked" : ""}
+                //         />
 
-                </section>
+                // </section>
         )
 
 }
